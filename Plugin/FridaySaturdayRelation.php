da077883bbc78b8e4adf464c5a062f51f269d9e9
Siarhey <siarhey.uchukhlebau@gmail.com>
2019-07-24 14:21:03 +0300

    Initial commit

---
+++ Plugin/FridaySaturdayRelation.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright © MageWorx. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace MageWorx\ExtendedConditionsDeliveryDate\Plugin;
+
+
+use DateTime;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use MageWorx\DeliveryDate\Api\DeliveryOptionInterface;
+
+class FridaySaturdayRelation
+{
+    /**
+     * @var TimezoneInterface
+     */
+    private $timezone;
+
+    /**
+     * FridaySaturdayRelation constructor.
+     *
+     * @param TimezoneInterface $timezone
+     */
+    public function __construct(
+        TimezoneInterface $timezone
+    ) {
+        $this->timezone = $timezone;
+    }
+
+    /**
+     * @param DeliveryOptionInterface $subject
+     * @param bool $result
+     * @param \DateTime $futureDate
+     * @return bool
+     * @throws \Exception
+     */
+    public function afterIsDayHoliday(DeliveryOptionInterface $subject, $result, $futureDate)
+    {
+        $today = $this->getCurrentDateTime();
+        if ($futureDate->format('l') === 'Saturday' &&
+            $today->format('l') === 'Friday' &&
+            $today->format('G') >= 21
+        ) {
+            $result = true;
+        }
+
+        if ($futureDate->format('l') === 'Thursday' &&
+            $today->format('l') === 'Wednesday' &&
+            $today->format('G') >= 12 &&
+            $today->diff($futureDate)->days < 2
+        ) {
+            $result = true;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return DateTime
+     * @throws \Exception
+     */
+    private function getCurrentDateTime()
+    {
+        $currentDayTime = new DateTime();
+        $storeTimeZone  = new \DateTimeZone($this->timezone->getConfigTimezone());
+        $currentDayTime->setTimezone($storeTimeZone);
+
+        return $currentDayTime;
+    }
+}
